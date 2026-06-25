97ff9ee1517a90b8dc90fa7c86ea9f07f288ba1d
CodexSandboxOffline <davisemmanule@gmail.com>
2026-06-25 09:58:59 -0500

    Add Workforce Management module

---
+++ workforce/worker-config.js
@@ -0,0 +1,6 @@
+window.IGEO_WORKFORCE = {
+  sheetId: "1RfKwfSjePnIQEFTsH1mcTbB16lGKPUKhkt4qtVqjh-c",
+  sheetName: "Worker Intake",
+  endpointUrl:
+    "https://script.google.com/macros/s/AKfycbzhXotHCZbZVCwde17SlXMvugPL3M8xbe15LEGicSfB6LAoJq8wub-qWFgUHdH6aCKI5Q/exec",
+};
