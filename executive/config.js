449dbf094cd30a705b6e69415d8df4e54909391f
davisemmanule-hue <davisemmanule@gmail.com>
2026-06-25 11:03:16 -0500

    Configure Executive Command Center cloud sources

---
+++ executive/config.js
@@ -0,0 +1,9 @@
+window.IGEO_EXECUTIVE = {
+  endpointUrl:
+    "https://script.google.com/macros/s/AKfycbwqXUElw5dNLrt5FYK6ddJ-ptBNm-RprgR3vznbKeZPIvpLQvG98HSavryAHhEOEbHu/exec",
+  primeEndpointUrl:
+    "https://script.google.com/macros/s/AKfycbyTccXyMv9_KMfQe9wFxlV8aNez7-T8efagw5TsTKOile_ZXCJ04ukXVLPunpHImju3sQ/exec",
+  workforceEndpointUrl:
+    "https://script.google.com/macros/s/AKfycbzhXotHCZbZVCwde17SlXMvugPL3M8xbe15LEGicSfB6LAoJq8wub-qWFgUHdH6aCKI5Q/exec",
+  githubUrl: "https://api.github.com/repos/davisemmanule-hue/iGeo-OS",
+};
